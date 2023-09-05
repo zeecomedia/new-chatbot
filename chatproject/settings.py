fa6fcbc9f47ae2dbe4432875ee940880cd890585
Isaac Abiodun <engrisaac1234@gmail.com>
2023-09-05 14:52:08 +0100

    fff

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+ckuu#+qp9g(n#_+q1^narr416nmgs5o0+eakr@^x1^l9sy43)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://iphonebot-15531d93cf3b.herokuapp.com/']
 
@@ -143,15 +143,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
-
-CSRF_TRUSTED_ORIGINS = [
-    'https://softbot.tk',
-    'https://www.softbot.tk',
-    "https://softbotday.herokuapp.com",
-]
