866842ec476ac30d103de6ae824b8c82c3125cd4
zeecomedia <151219906+zeecomedia@users.noreply.github.com>
2023-11-18 08:52:24 +0100

    Update settings.py

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,5 +147,3 @@ STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
